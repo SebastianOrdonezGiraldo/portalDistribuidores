8e65f7a3ee7ee335bf8cca07209b8cf2473df249
Sebastian <sebastian789go@gmail.com>
2026-03-27 15:45:50 -0500

    fix: update phone input placeholder format in checkout form

--- resources/views/orders/checkout.blade.php
+++ resources/views/orders/checkout.blade.php
@@ -73,7 +73,7 @@
                 </div>
                 <div>
                     <label class="form-label" for="phone">Teléfono *</label>
-                    <x-ui.input id="phone" type="tel" name="phone" :value="old('phone', $distributor?->phone)" placeholder="+57 300 000 0000" required />
+                    <x-ui.input id="phone" type="tel" name="phone" :value="old('phone', $distributor?->phone)" placeholder="300 000 0000" required />
                     <x-input-error :messages="$errors->get('phone')" />
                 </div>
                 <div>
