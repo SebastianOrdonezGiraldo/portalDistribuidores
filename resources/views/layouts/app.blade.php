7cfcd93c9dd23e93480edf7f17345f6a2c29df77
Sebastian <sebastian789go@gmail.com>
2026-03-31 11:40:15 -0500

    feat: enhance catalog UI with new toolbar and filter components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -251,7 +251,7 @@
                 </div>
 
                 @isset($catalogToolbar)
-                    <div class="border-t border-slate-200/80 pt-3">
+                    <div class="pt-2">
                         {{ $catalogToolbar }}
                     </div>
                 @elseif($isAdmin)
